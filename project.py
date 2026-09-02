da0cb61daecb66ccc0abf6043433c603080c1aa4
ashilbaniya <ph4ntom.eclip5e@gmail.com>
2026-09-02 22:00:15 +0545

    updated project.py

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 from utils import read, write
 from trackers import Tracker
+import cv2
 
 def main():
     # Read the video
@@ -8,6 +9,18 @@ def main():
     tracker = Tracker('models/best.pt')
     tracks = tracker.get_object_tracks(cap_frames, read_from_stub=True,
                                         stub_path='./stubs/track_stubs.pkl')
+
+    # Cropping a player out of a frame
+    # for track_id, player in tracks["players"][0].items():
+    #     bbox = player['bbox']
+    #     frame = cap_frames[0]
+
+    #     cropped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+
+    #     # Saving the cropped image
+    #     cv2.imwrite(f'cropped_images/image_{track_id}.png', cropped_image)
+
+    #     break
 
     output_video_frames = tracker.draw_annotations(cap_frames,tracks)
 
