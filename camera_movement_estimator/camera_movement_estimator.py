c307aeb5001a0e3420fec044aa08457dc076b24e
ashilbaniya <ph4ntom.eclip5e@gmail.com>
2026-09-03 16:36:43 +0545

    added camera_movement_estimator directory and a .py file to track the camera movements in the video

---
+++ camera_movement_estimator/camera_movement_estimator.py
@@ -0,0 +1,42 @@
+import os
+import cv2
+import pickle
+import numpy as np
+
+class CameraMovementEstimator:
+    def __init__(self, frame):
+        first_frame_grayscale = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        mask_features = np.zeros_like(first_frame_grayscale)
+        mask_features[:,0:20] = 1
+        mask_features[:,900:1050] = 1
+
+        self.lk_params = dict(
+            winSize = (15,15),
+            maxLevel = 2,
+            criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03)
+        )
+
+        self.features = dict(
+            maxCorners = 100,
+            qualityLevel = 0.3,
+            minDistance = 3,
+            blockSize = 7,
+            mask = mask_features
+        )
+
+    def get_camera_movement(self, frames, read_from_stub=False, stub_path=None):
+        if read_from_stub and stub_path is not None and os.path.exists(stub_path):
+            # read the stub
+            ...
+
+        camera_movement = [[0,0]*len(frames)]
+
+        old_gray = cv2.cvtColor(frames[0], cv2.COLOR_RGB2GRAY)
+        old_features = cv2.goodFeaturesToTrack(old_gray,**self.features)
+
+        for frame_num in range(1,len(frames)):
+            frame_gray = cv2.cvtColor(frames[frame_num], cv2.COLOR_RGB2GRAY)
+            new_features,_,_ = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, old_features, None, **self.lk_params)
+
+            max_distance = 0
+            camera_movement_x, camera_movement_y = 0,0
