5feca777118e991506d689f74d5ec76c8fab8575
ashilbaniya <ph4ntom.eclip5e@gmail.com>
2026-09-03 17:49:19 +0545

    created camera movement estimator to track the movement of the frame and display it

--- camera_movement_estimator/camera_movement_estimator.py
+++ camera_movement_estimator/camera_movement_estimator.py
@@ -1,7 +1,11 @@
 import os
 import cv2
+import sys
 import pickle
 import numpy as np
+sys.path.append('../')
+from utils import measure_distance, measure_xy_distance
+
 
 class CameraMovementEstimator:
     def __init__(self, frame):
@@ -9,6 +13,7 @@ class CameraMovementEstimator:
         mask_features = np.zeros_like(first_frame_grayscale)
         mask_features[:,0:20] = 1
         mask_features[:,900:1050] = 1
+        self.minimum_distance = 5
 
         self.lk_params = dict(
             winSize = (15,15),
@@ -25,11 +30,13 @@ class CameraMovementEstimator:
         )
 
     def get_camera_movement(self, frames, read_from_stub=False, stub_path=None):
-        if read_from_stub and stub_path is not None and os.path.exists(stub_path):
-            # read the stub
-            ...
 
-        camera_movement = [[0,0]*len(frames)]
+        if read_from_stub and stub_path is not None and os.path.exists(stub_path):
+            # read from the stub
+            with open(stub_path, 'rb') as f:
+                pickle.load(f)
+
+        camera_movement = [[0,0]]*len(frames)
 
         old_gray = cv2.cvtColor(frames[0], cv2.COLOR_RGB2GRAY)
         old_features = cv2.goodFeaturesToTrack(old_gray,**self.features)
@@ -40,3 +47,44 @@ class CameraMovementEstimator:
 
             max_distance = 0
             camera_movement_x, camera_movement_y = 0,0
+
+            for i, (new, old) in enumerate(zip(new_features, old_features)):
+                new_features_points = new.ravel()
+                old_features_points = old.ravel()
+
+                distance = measure_distance(new_features_points, old_features_points)
+                if distance > max_distance:
+                    max_distance = distance
+                    camera_movement_x, camera_movement_y = measure_xy_distance(old_features_points, new_features_points)
+
+            if max_distance > self.minimum_distance:
+                camera_movement[frame_num] = [camera_movement_x, camera_movement_y]
+                old_features = cv2.goodFeaturesToTrack(frame_gray, **self.features)
+
+            old_gray = frame_gray.copy()
+
+        if stub_path is not None:
+            with open(stub_path, 'wb') as f:
+                pickle.dump(camera_movement, f)
+
+        return camera_movement
+
+    def draw_camera_movement(self, frames, camera_movement_per_frame):
+        output_frames = []
+
+        for frame_num, frame in enumerate(frames):
+            frame = frame.copy()
+
+            overlay = frame.copy()
+            cv2.rectangle(overlay, (0,60), (500,140), (255,255,255), cv2.FILLED)
+            alpha = 0.4
+            cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0, frame)
+
+            x_movement, y_movement = camera_movement_per_frame[frame_num]
+
+            frame = cv2.putText(frame, f"X movement: {x_movement:.2f}", (60,90), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,0), 3)
+            frame = cv2.putText(frame, f"Y movement: {y_movement:.2f}", (60,135), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,0), 3)
+
+            output_frames.append(frame)
+
+        return output_frames
