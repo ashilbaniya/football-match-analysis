6f73c701754ab22163cfe8b58a428da33817895b
ashilbaniya <ph4ntom.eclip5e@gmail.com>
2026-09-03 21:56:56 +0545

    updated project.py

--- project.py
+++ project.py
@@ -3,6 +3,8 @@ from trackers import Tracker
 from team_assigner import TeamAssigner
 from player_ball_assigner import PlayerBallAssigner
 from camera_movement_estimator import CameraMovementEstimator
+from view_transformer import ViewTransformer
+from speed_and_distance_estimator import SpeedAndDistanceEstimator
 import numpy as np
 import cv2
 
@@ -14,13 +16,27 @@ def main():
     tracks = tracker.get_object_tracks(cap_frames, read_from_stub=True,
                                         stub_path='./stubs/track_stubs.pkl')
 
-    tracks["ball"] = tracker.interpolate_ball_position(tracks["ball"])
+    # Get object positions
+    tracker.add_position_to_tracks(tracks)
+
 
     # camera movement estimator
     camera_movement_estimator = CameraMovementEstimator(cap_frames[0])
     camera_movement_per_frame = camera_movement_estimator.get_camera_movement(cap_frames,
                                                                             read_from_stub=True,
                                                                             stub_path='./stubs/camera_movement_stub.pkl')
+
+    camera_movement_estimator.add_adjust_positions_to_tracks(tracks, camera_movement_per_frame)
+
+    # View Transformer
+    view_transformer = ViewTransformer()
+    view_transformer.add_transformed_position_to_tracks(tracks)
+
+    tracks["ball"] = tracker.interpolate_ball_position(tracks["ball"])
+
+    # Speed and Distance Estimator
+    speed_and_distance_estimator = SpeedAndDistanceEstimator()
+    speed_and_distance_estimator.add_speed_and_distance_to_tracks(tracks)
 
     # Assigning player teams
     team_assigner = TeamAssigner()
@@ -57,8 +73,13 @@ def main():
     # Draw camera movements
     output_video_frames = camera_movement_estimator.draw_camera_movement(output_video_frames, camera_movement_per_frame)
 
+    # Draw speed and distance
+    output_video_frames = speed_and_distance_estimator.draw_speed_and_distance(output_video_frames, tracks)
+
     # Save the video
     write(output_video_frames, 'output_videos/out.avi')
+    print("Program execucted successfully.")
+    print("Output saved to 'output_videos/out.avi.")
 
 if __name__ == '__main__':
     main()
